6a6183de9074f71dfaeeaf6728915012883bba86
Tonteuy <tonteuy@hotmail.com>
2026-03-31 22:12:44 +0700

    แก้ prod

--- js/core/supabaseClient.js
+++ js/core/supabaseClient.js
@@ -13,8 +13,8 @@ if (typeof supabase === 'undefined') {
 // สร้าง Supabase Client
 // ใช้ตัวแปร global เพื่อให้เข้าถึงได้จากทุกไฟล์
 const supabaseClient = supabase.createClient(
-  "https://vhazgytcfvjhhikiqpwm.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoYXpneXRjZnZqaGhpa2lxcHdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NjA1MjgsImV4cCI6MjA4NjIzNjUyOH0.wHHUPop0xMrUgX6X8Jkk-fahVfIMW-iYx4NT0zg5lxM"
+  "https://kdgmilagtpizwnhwapgl.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkZ21pbGFndHBpenduaHdhcGdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NTYzMjgsImV4cCI6MjA5MDQzMjMyOH0.v-TtDtF7RfwxA-qxpvIBquTI8lBaTkiHQ-M7Maf5jeU"
 );
 
 
