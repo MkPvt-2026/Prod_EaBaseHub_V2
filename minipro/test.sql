7da45ace5d5204d2203b928304bf297ac8474d65
Tonteuy <tonteuy@hotmail.com>
2026-04-29 21:22:06 +0700

    test

---
+++ minipro/test.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sales (
+    id INT,
+    employee_name TEXT,
+    province TEXT,
+    sales_value INT
+);
+INSERT INTO sales
+VALUES (1, 'ต้นเตย', 'กรุงเทพ', 5000);
+SELECT * FROM sales;
